844086005aef2483b93ab743a0918a2671cbe998
Anna Ryazantceva <anyariazantzeva@yandex.ru>
2020-02-22 19:26:20 +0200

    Created Header html layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Logo from '../assets/Logo.png';
+import './Header.css';
 
-const Header = () => {
+const Header = ({ userName }) => {
     return (
-        <div>Hello</div>
+        <header className='header'>
+            <div className="header__logo">
+                <img src={Logo} alt=""/>
+            </div>
+            <div className="header__user">
+                {userName}
+            </div>
+            <div className="header__languages">
+                <select name="languages" className="ui dropdown">
+                    <option value="">Language</option>
+                    <option value="English">English</option>
+                    <option value="Finnish">Finnish</option>
+                    <option value="Swedish">Swedish</option>
+                </select>
+            </div>
+        </header>
     )
 };
 
